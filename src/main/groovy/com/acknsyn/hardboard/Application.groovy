045b289061afda35e77e809c79a7a855e0718e00
Brandon Atkinson <brandon-atkinson@idexx.com>
2015-01-29 09:17:00 -0500

    Removed mongo related stuff (h2 all the way baby)

--- src/main/groovy/com/acknsyn/hardboard/Application.groovy
+++ src/main/groovy/com/acknsyn/hardboard/Application.groovy
@@ -2,14 +2,12 @@ package com.acknsyn.hardboard
 
 import org.springframework.context.annotation.*
 import org.springframework.boot.autoconfigure.*
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 import static org.springframework.boot.SpringApplication.run
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableMongoRepositories
 class Application{
     static void main(args) {
         run(Application, args)
