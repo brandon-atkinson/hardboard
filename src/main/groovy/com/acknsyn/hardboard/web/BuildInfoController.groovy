81e5093489581079a8d53056d8c73887d15eb380
Brandon Atkinson <brandon-atkinson@idexx.com>
2015-01-30 11:05:58 -0500

    Updated visualization code to update build if already present

--- src/main/groovy/com/acknsyn/hardboard/web/BuildInfoController.groovy
+++ src/main/groovy/com/acknsyn/hardboard/web/BuildInfoController.groovy
@@ -1,10 +1,8 @@
 package com.acknsyn.hardboard.web
-
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-
 /**
  *
  */
