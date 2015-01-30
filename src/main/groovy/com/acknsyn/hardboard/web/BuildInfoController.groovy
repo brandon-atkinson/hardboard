2f6bdc04ec4a1468356045e67834bb9e758a56bf
Brandon Atkinson <brandon-atkinson@idexx.com>
2015-01-30 08:45:06 -0500

    Added build info service and index page that displays it

---
+++ src/main/groovy/com/acknsyn/hardboard/web/BuildInfoController.groovy
@@ -0,0 +1,26 @@
+package com.acknsyn.hardboard.web
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ *
+ */
+@RestController
+class BuildInfoController {
+    @Value("#{environment['info.build.number']}")
+    String build
+
+    @RequestMapping(value = "/build-info", method = RequestMethod.GET)
+    Map buildInfo() {
+        String instance
+        try {
+            instance = new URL('http://169.254.169.254/latest/meta-data/instance-id').getText()
+        } catch (e) {
+            instance = InetAddress.getLocalHost().hostName
+        }
+        return [host:instance, build:build]
+    }
+}
